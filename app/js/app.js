1c30232dcd57d8f5c9c17bdec94e0ec7c43b3d08
kyleconkright <kyleconkright@gmail.com>
2016-05-02 17:22:01 -0700

    added templates

--- app/js/app.js
+++ app/js/app.js
@@ -30,6 +30,23 @@ angular.module('app')
 	      });
 	    }
 	  };
+	})
+	.directive('homeNav', function($location) {
+	  return {
+	    restrict: 'A',
+	    scope: {
+	      path: "@homeNav"
+	    },
+	    link: function(scope, element, attributes) {
+	      scope.$on('$locationChangeSuccess', function() {
+	        if ($location.path() === scope.path) {
+	          element.addClass('light');
+	        } else {
+	          element.removeClass('light');
+	        }
+	      });
+	    }
+	  };
 	});
 
 
--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-    // jQuery('.slider').slick();
+    jQuery('.slider').slick();
 }

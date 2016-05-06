bc78cda60fdd5a4bcbb89abe2712dadec8052696
kyleconkright <kyleconkright@gmail.com>
2016-05-06 13:19:03 -0700

    prepare for remote server access

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var http = require('http');
 
 var app = express();
 
@@ -9,6 +10,14 @@ app.use(express.static(path.join(__dirname, 'app')));
 
 //SLIDES API
 app.get('/api', function(req, res) {
+	// http.get('http://www.kyleconkright.com/seetickets', function(response) {
+	//   console.log(response.statusCode);
+	//   // consume response body
+	//   response.resume();
+	// }).on('error', function(e) {
+	//   console.log(e.message);
+	// });
+	// var files = fs.readdirSync('http://www.kyleconkright.com/seetickets');
 	var files = fs.readdirSync(__dirname+'/app/assets/slides/');
 	var slides = {slides: []};
 	for(var i in files) {

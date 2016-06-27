d72e231f489c188adec85be1325021b389ebbac2
kyleconkright <kyleconkright@gmail.com>
2016-06-27 10:23:53 -0700

    added b bold style

--- public/app/app.js
+++ public/app/app.js
@@ -159,13 +159,8 @@
 		.directive('about', function(){
 			return {
 				restrict: "A",
-				controller: ['$scope', '$http', '$sce', 'ngProgressLite', function($scope, $http, $sce, ngProgressLite){
-					ngProgressLite.start();
-					$http.get('/api/about').success(function(response){
-						// $scope.about = response;
-						$scope.about = $sce.trustAsHtml(response);
-						ngProgressLite.done();
-					});
+				controller: ['$scope', 'ngProgressLite', function($scope, ngProgressLite){
+					$scope.about = 'hello';
 				}]
 			}
 		})
